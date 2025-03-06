97f8810c61fe2198841ecdb35af382ac36bbd44e
Andrew Dong <andrewdong1994@gmail.com>
2025-03-06 15:49:24 -0500

    Update endpoint configuration for Fly.io deployment

--- config/prod.exs
+++ config/prod.exs
@@ -1,5 +1,18 @@
 import Config
 
+# For production, don't forget to configure the url host
+# to something meaningful, Phoenix uses this information
+# when generating URLs.
+
+# Configuring the endpoint
+config :sorting_hat, SortingHatWeb.Endpoint,
+  url: [host: "sorting-octopus.fly.dev", port: 80],
+  http: [
+    ip: {0, 0, 0, 0},
+    port: 8080
+  ],
+  cache_static_manifest: "priv/static/cache_manifest.json"
+
 # Do not print debug messages in production
 config :logger, level: :info
 
